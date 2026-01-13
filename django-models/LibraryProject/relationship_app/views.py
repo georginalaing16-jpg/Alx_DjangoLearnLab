62af01b0c99a89c2ead88138df1bfb0322e8f69a
Georgina Ferguson Laing <georginalaing16@gmail.com>
2026-01-13 17:14:19 +0000

    Add changes

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,7 +4,8 @@ from django.views.generic import DetailView
 from .models import Book, Library
 
 def list_books(request):
-    books = Book.objects.select_related('author').all()
+    books = Book.objects.all()
+
     return render(request, 'relationship_app/list_books.html', {
         'books': books
     })
