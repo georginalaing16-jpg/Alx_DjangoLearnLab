0c99a0d5818af3240e9cb9dbc1c8285b26532cdd
Georgina Ferguson Laing <georginalaing16@gmail.com>
2026-01-13 20:21:57 +0000

    Add login.html and logout.html

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -8,3 +8,12 @@ urlpatterns = [
     # Class-based view
     path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
+
+from django.urls import path
+from .views import CustomLoginView, CustomLogoutView, register
+
+urlpatterns = [
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('register/', register, name='register'),
+]
