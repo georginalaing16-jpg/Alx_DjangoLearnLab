91233b63ac9ec510f8d1cb84de055b739f2987fc
Georgina Ferguson Laing <georginalaing16@gmail.com>
2026-01-13 21:02:31 +0000

    Add admin_view.html, librarian_view.html, member_view.html

--- django-models/LibraryProject/relationship_app/apps.py
+++ django-models/LibraryProject/relationship_app/apps.py
@@ -3,3 +3,12 @@ from django.apps import AppConfig
 
 class RelationshipAppConfig(AppConfig):
     name = 'relationship_app'
+
+from django.apps import AppConfig
+
+class RelationshipAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'relationship_app'
+
+    def ready(self):
+        import relationship_app.signals
