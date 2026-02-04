92fddceb4a68aaa1138eb4fef812f67aadb0a257
Georgina Ferguson Laing <georginalaing16@gmail.com>
2026-02-04 18:46:58 +0000

    Add test_views to api

---
+++ advanced-api-project/api/test_views.py
@@ -0,0 +1,31 @@
+from rest_framework.test import from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
+from api.models import Author, Book
+
+# Unit tests for book API endpoints
+class BookAPITestCase(APITestCase):
+
+    def setUp(self):
+        # Create test user
+        self.user = User.objects.create_user(
+            username="testuser",
+            password="testpassword"
+        )
+
+        # Create author
+        self.author = Author.objects.create(name="Test Author")
+
+        # Create books
+        self.book1 = Book.objects.create(
+            title="Test Book 1",
+            publication_year=2025,
+            author=self.author
+        )
+
+        # API endpoints
+        self.list_url = reverse("book-list")
+        self.detail_url = reverse("book-detail", args=[self.book1.id])
+
+
