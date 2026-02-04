e635fce5a699b15e0d144322dea8ebce14ae65fc
Georgina Ferguson Laing <georginalaing16@gmail.com>
2026-02-04 19:03:49 +0000

    Add test_views.py to api

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -29,3 +29,40 @@ class BookAPITestCase(APITestCase):
         self.detail_url = reverse("book-detail", args=[self.book1.id])
 
 
+    def test_create_book_authenticated(self):
+        self.client.login(username="testuser", password="testpassword")
+        data = {
+            "title": "New Book",
+            "publication_year": 2023,
+            "author": self.author.id
+        }
+        response = self.client.post(reverse("book-create"), data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data["book"]["title"], "Test Book")
+
+    
+    def test_create_book_unauthenticated(self):
+        data = {
+            "title": "Unauthenticated Book",
+            "publication_year": 2023,
+            "author": self.author.id
+        }
+        response = self.client.post(self.list_url, data)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+        
+    def test_update_book_authenticated(self):
+        self.client.login(username="testuser", password="testpassword")
+        data = {
+            "title": "Updated Book Title"
+        }
+        response = self.client.put(self.detail_url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["book"]["title"], "Updated Book Title")
+
+
+        def test_delete_book_unauthenticated(self):
+            response = self.client.delete(self.detail_url)
+
+            self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+            self.assertEqual(Book.objects.count(), 1)
