6d824438d2ebd4e20747fa9025d7777ab7ebda7d
Georgina Ferguson Laing <georginalaing16@gmail.com>
2026-02-11 18:56:05 +0000

    Add user authentication set up to django_blog

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -121,11 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_URL = 'blog:login'
+LOGIN_REDIRECT_URL = 'blog:profile'
+LOGOUT_REDIRECT_URL = 'blog:login'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-]
+] 
 
-LOGIN_URL = 'login'
-LOGIN_REDIRECT_URL = 'profile'
-LOGOUT_REDIRECT_URL = 'login'
