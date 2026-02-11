6a682d3c2c2bbeeb04243d5d3067240f19b676d6
Georgina Ferguson Laing <georginalaing16@gmail.com>
2026-02-11 19:55:07 +0000

    Add user authentication to django_blog

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
 LOGIN_URL = 'blog:login'
 LOGIN_REDIRECT_URL = 'blog:profile'
 LOGOUT_REDIRECT_URL = 'blog:login'
