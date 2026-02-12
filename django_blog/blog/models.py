7e9abb3e7a2df7e614f7ac9f13373cd04e63f424
Georgina Ferguson Laing <georginalaing16@gmail.com>
2026-02-12 00:23:34 +0000

    Add tagging and search functionality to django_blog

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -19,7 +19,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
 
     tags = models.ManyToManyField(Tag, related_name='posts', blank=True)
-    
+
     def __str__(self):
         return self.title
     
@@ -53,3 +53,16 @@ class Comment(models.Model):
     def get_absolute_url(self):
         # After create/update/delete, return to the post detail page
         return reverse("blog:post_detail", kwargs={"pk": self.post.pk})
+
+
+
+from taggit.managers import TaggableManager
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    tags = TaggableManager()
+
