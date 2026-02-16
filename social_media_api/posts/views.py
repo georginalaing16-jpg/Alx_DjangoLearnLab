54cb9d718458aa0fe837e6d03190fec64cdefe5b
Georgina Ferguson Laing <georginalaing16@gmail.com>
2026-02-16 20:32:00 +0000

    Add user follows and feed functionality to social_media_api

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -34,3 +34,20 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+
+
+
+from rest_framework import permissions
+from rest_framework.generics import ListAPIView
+from .models import Post
+from .serializers import PostSerializer
+
+
+class FeedView(ListAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        following_users = self.request.user.following.all()
+        return Post.objects.filter(author__in=following_users).order_by("-created_at")
